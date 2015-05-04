0ad6d55cb87d79f1011649ab60a7f816c4f908b8
Etienne Cimon <etcimon@gmail.com>
2015-05-04 16:25:53 -0400

    Add license headers

--- source/vibe/http/client.d
+++ source/vibe/http/client.d
@@ -3,7 +3,7 @@
 
 	Copyright: © 2012-2014 RejectedSoftware e.K.
 	License: Subject to the terms of the MIT license, as written in the included LICENSE.txt file.
-	Authors: Sönke Ludwig, Jan Krüger
+	Authors: Sönke Ludwig, Jan Krüger, Etienne Cimon
 */
 module vibe.http.client;
 
